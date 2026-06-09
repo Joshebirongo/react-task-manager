7d7984f70eb316c5dc68e8aba2eb802def0ff76b
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-06-09 15:56:51 +0200

    App.jsx created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-function App() {
-  return <></>;
-}
+import { Router, Routes } from "react-router-dom";
 
-export default App;
+// Import the components and pages
+import Navbar from "./components/Navbar";
+// Import the pages
+import Home from "./pages/Home";
+import Tasks from "./pages/Tasks";
+import TaskDetails from "./pages/TaskDetails";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tasks" element={<Tasks />}></Route>
+        <Route path="/tasks/:id" element={<TaskDetails />}></Route>
+        <Route path="/about" element={<Aboute />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </>
+  );
+}
