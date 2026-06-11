39ae613b56803ad812a8bea896176a7f802e2a6f
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-06-11 18:16:35 +0200

    Page Notifications created

---
+++ src/pages/Notifications.jsx
@@ -0,0 +1,12 @@
+import PageHeader from "../components/PageHeader";
+import NotifcationList from "../components/NotificationList";
+
+export default function Notifcations() {
+  return (
+    <div>
+      <PageHeader title="Notifications" />
+
+      <NotifcationList />
+    </div>
+  );
+}
