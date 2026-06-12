a543c9868c7d4ddc1083d36f2ae4c43de4c4d65e
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-06-12 18:02:16 +0200

    Dataset ContactData created

---
+++ src/data/contactData.js
@@ -0,0 +1,8 @@
+const contactData = {
+  CompanyName: "PrimeTek Africa",
+  phoneNumber: "+243 997 300 904",
+  mailAddress: "info@primetek.africa",
+  linkedIn: "https://www.linkedin.com/company/primetekafrica",
+};
+
+export default contactData;
