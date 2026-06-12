93ab25dd2adb67bed123ec9f876b1347ef8c2806
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-06-12 17:07:48 +0200

    Dataset activities created

---
+++ src/data/activities.js
@@ -0,0 +1,10 @@
+const activities = [
+  "Create Dashboard",
+  "Added Router",
+  "Create Admin panel",
+  "Update settings page",
+  "Fix the profile page",
+  "Improve the components styles",
+];
+
+export default activities;
