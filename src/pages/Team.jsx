3752d8e79cc33e7f224e0fe58ff8b5df5aa8e5b3
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-06-12 16:30:59 +0200

    Page Team created

---
+++ src/pages/Team.jsx
@@ -0,0 +1,11 @@
+import TeamList from "../components/TeamList";
+import PageHeader from "../components/PageHeader";
+
+export default function Team() {
+  return (
+    <div>
+      <PageHeader title="Team" />
+      <TeamList />
+    </div>
+  );
+}
