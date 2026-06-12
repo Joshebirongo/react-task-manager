8fc2cb41a495362544c0a30885078e6e1bb5a15d
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-06-12 18:01:43 +0200

    Page Help created

---
+++ src/pages/Help.jsx
@@ -0,0 +1,29 @@
+//Import Dataset
+import contactData from "../data/contactData";
+// Import components
+import PageHeader from "../components/PageHeader";
+
+export default function Help() {
+  return (
+    <div>
+      <PageHeader title="Help" />
+
+      <p>Find answers to common questions.</p>
+      <br />
+
+      <article>
+        <p>
+          Company name: <strong>{contactData.CompanyName}</strong>
+        </p>
+        <p>
+          Phone number: <strong>{contactData.phoneNumber}</strong>
+        </p>
+        <p>
+          Mail address: <strong>{contactData.mailAddress}</strong>
+        </p>
+
+        <a href={contactData.linkedIn}>LinkedIn account</a>
+      </article>
+    </div>
+  );
+}
