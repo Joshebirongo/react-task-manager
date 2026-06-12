6acd8bd524e0f08ede53986f20524aed8f66cb78
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-06-12 16:31:25 +0200

    App.jsx updated - Team

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Tasks from "./pages/Tasks";
 import TasksMenu from "./pages/TasksMenu";
 import CreateTask from "./pages/CreateTask";
 import TaskDetails from "./pages/TaskDetails";
+import Team from "./pages/Team";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
@@ -35,6 +36,8 @@ export default function App() {
         <Route path="/tasks/new" element={<CreateTask />} />
 
         <Route path="/notifications" element={<Notifcations />} />
+
+        <Route path="/team" element={<Team />} />
 
         <Route path="/profile" element={<Profile />} />
 
