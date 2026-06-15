9e79d896f88cd8daeaf45c7d1a704fcd59a1aa60
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-06-15 12:18:56 +0200

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,51 +7,51 @@ export default function Navbar() {
       <Link to="/" className="nav-link">
         Home
       </Link>
-      <br />
+
       <Link to="/dashboard" className="nav-link">
         Dashboard
       </Link>
-      <br />
+
       <Link to="/tasks" className="nav-link">
         Tasks
       </Link>
-      <br />
+
       <Link to="/tasks/menu" className="nav-link">
         Tasks Menu
       </Link>
-      <br />
+
       <Link to="/tasks/new" className="nav-link">
         Create Task
       </Link>
-      <br />
+
       <Link to="/notifications" className="nav-link">
         Notifications
       </Link>
-      <br />
+
       <Link to="/activities" className="nav-link">
         Activities
       </Link>
-      <br />
+
       <Link to="/profile" className="nav-link">
         Profile
       </Link>
-      <br />
+
       <Link to="/team" className="nav-link">
         Team
       </Link>
-      <br />
+
       <Link to="/settings" className="nav-link">
         Settings
       </Link>
-      <br />
+
       <Link to="/reports" className="nav-link">
         Reports
       </Link>
-      <br />
+
       <Link to="/about" className="nav-link">
         About
       </Link>
-      <br />
+
       <Link to="/help" className="nav-link">
         Help
       </Link>
